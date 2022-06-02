982e4525011bcf7573b8ec6f47ff57084ef74f09
Bùi Hữu Lộc <feature451@gmail.com>
2022-06-02 17:09:31 +0700

    longer field name

--- database/migrations/2022_06_01_082832_create_storages_table.php
+++ database/migrations/2022_06_01_082832_create_storages_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('storages', function (Blueprint $table) {
             $table->id();
             $table->text('content')->nullable();
-            $table->string('type', 250)->nullable();
-            $table->string('name', 250)->nullable();
+            $table->string('type', 50)->nullable();
+            $table->text('name')->nullable();
             $table->timestamps();
         });
     }
