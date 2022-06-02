2c91ac3f98f7bc0975dedfa213424fbf5e7c3615
Bùi Hữu Lộc <feature451@gmail.com>
2022-06-02 20:30:48 +0700

    new feature: upload file

--- app/Http/Controllers/StorageController.php
+++ app/Http/Controllers/StorageController.php
@@ -36,18 +36,28 @@ class StorageController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // Lấy data
         $data = $request->all();
-
         if (empty($data['content'])) {
             return redirect()->back();
         }
-
         $content = $data['content'];
         $name = $data['name'];
         $type = empty($data['type']) ? "other" : "mlem";
         $image = getimagesize($content);
 
-        //
+        // Nếu upload ảnh bằng form
+        if (isset($data['file_image'])) {
+            $image_name = StorageFile::disk('public')->putFile("src/$type", $data['file_image']);
+            Storage::query()->create([
+                'content' => "public/" . $image_name,
+                'type' => $type,
+                'name' => $name
+            ]);
+            return redirect()->route('index');
+        }
+
+        // Nếu không upload ảnh
         if ($image === false) {
             Storage::query()->create([
                 'content' => $content,
@@ -57,26 +67,24 @@ class StorageController extends Controller
             return redirect()->route('index');
         }
 
+        // Nếu upload ảnh bằng input text
         $image_name = "";
         if ($image['mime'] === "image/png") {
-            $image_name = "public/src/$type/" . uniqid('image_', true) . '.png';
+            $image_name = "public/src/$type/" . uniqid('', true) . '.png';
         }
         if ($image['mime'] === "image/jpeg") {
-            $image_name = "public/src/$type/" . uniqid('image_', true) . '.jpg';
+            $image_name = "public/src/$type/" . uniqid('', true) . '.jpg';
         }
         if ($image['mime'] === "image/webp") {
-            $image_name = "public/src/$type/" . uniqid('image_', true) . '.webp';
+            $image_name = "public/src/$type/" . uniqid('', true) . '.webp';
         }
+        StorageFile::put($image_name, file_get_contents($content));
         Storage::query()->create([
             'content' => $image_name,
             'type' => $type,
             'name' => $name
         ]);
-        StorageFile::put($image_name, file_get_contents($content));
-
-
         return redirect()->route('index');
-
 
     }
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
 @if ($is_mobile)
-    <link rel="stylesheet" href="{{asset('css/style-mobile.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style-mobilee.css')}}">
 @else
-    <link rel="stylesheet" href="{{asset('css/style-desktop1.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style-desktopp.css')}}">
 @endif
 
 
 <div class="login-box">
     <h1>STORAGE APP</h1>
-    <form action="{{route('store')}}" method="post">
+    <form action="{{route('store')}}" method="post" id="file-upload-form" class="uploader" enctype="multipart/form-data">
         @csrf
 
         <div class="user-box">
@@ -35,14 +35,37 @@
             <div id="subscribe">NO</div>
             <div id="alright"> YES</div>
         </label>
-        <div style="
-            margin: 0;
-            position: absolute;
-            top: 80%;
-            left: 50%;
-            -ms-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-        ">
+
+{{--        --}}
+        <input id="file-upload" type="file" name="file_image" accept="image/*" />
+
+        <label for="file-upload" id="file-drag" class="label_upload">
+            <img id="file-image" src="#" alt="Preview" class="hidden">
+            <div id="start">
+                <i class="fa fa-download" aria-hidden="true"></i>
+                <div>Select a file or drag here</div>
+                <div id="notimage" class="hidden">Please select an image</div>
+                <span id="file-upload-btn" class="btn btn-primary">Select a file</span>
+            </div>
+            <div id="response" class="hidden">
+                <div id="messages"></div>
+                <progress class="progress" id="file-progress" value="0">
+                    <span>0</span>%
+                </progress>
+            </div>
+        </label>
+
+
+
+{{--        --}}
+{{--        <div style="--}}
+{{--            margin: 0;--}}
+{{--            position: absolute;--}}
+{{--            top: 80%;--}}
+{{--            left: 50%;--}}
+{{--            -ms-transform: translate(-50%, -50%);--}}
+{{--            transform: translate(-50%, -50%);--}}
+{{--        ">--}}
             <button type="submit">
                 <span></span>
                 <span></span>
@@ -50,8 +73,154 @@
                 <span></span>
                 STORE
             </button>
-        </div>
+{{--        </div>--}}
 
 
     </form>
 </div>
+
+
+<script type="text/javascript">
+    // File Upload
+    //
+    function ekUpload(){
+        function Init() {
+
+            console.log("Upload Initialised");
+
+            var fileSelect    = document.getElementById('file-upload'),
+                fileDrag      = document.getElementById('file-drag'),
+                submitButton  = document.getElementById('submit-button');
+
+            fileSelect.addEventListener('change', fileSelectHandler, false);
+
+            // Is XHR2 available?
+            var xhr = new XMLHttpRequest();
+            if (xhr.upload) {
+                // File Drop
+                fileDrag.addEventListener('dragover', fileDragHover, false);
+                fileDrag.addEventListener('dragleave', fileDragHover, false);
+                fileDrag.addEventListener('drop', fileSelectHandler, false);
+            }
+        }
+
+        function fileDragHover(e) {
+            var fileDrag = document.getElementById('file-drag');
+
+            e.stopPropagation();
+            e.preventDefault();
+
+            fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload');
+        }
+
+        function fileSelectHandler(e) {
+            // Fetch FileList object
+            var files = e.target.files || e.dataTransfer.files;
+
+            // Cancel event and hover styling
+            fileDragHover(e);
+
+            // Process all File objects
+            for (var i = 0, f; f = files[i]; i++) {
+                parseFile(f);
+                uploadFile(f);
+            }
+        }
+
+        // Output
+        function output(msg) {
+            // Response
+            var m = document.getElementById('messages');
+            m.innerHTML = msg;
+        }
+
+        function parseFile(file) {
+
+            console.log(file.name);
+            output(
+                '<strong>' + encodeURI(file.name) + '</strong>'
+            );
+
+            // var fileType = file.type;
+            // console.log(fileType);
+            var imageName = file.name;
+
+            var isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName);
+            if (isGood) {
+                document.getElementById('start').classList.add("hidden");
+                document.getElementById('response').classList.remove("hidden");
+                document.getElementById('notimage').classList.add("hidden");
+                // Thumbnail Preview
+                document.getElementById('file-image').classList.remove("hidden");
+                document.getElementById('file-image').src = URL.createObjectURL(file);
+            }
+            else {
+                document.getElementById('file-image').classList.add("hidden");
+                document.getElementById('notimage').classList.remove("hidden");
+                document.getElementById('start').classList.remove("hidden");
+                document.getElementById('response').classList.add("hidden");
+                document.getElementById("file-upload-form").reset();
+            }
+        }
+
+        function setProgressMaxValue(e) {
+            var pBar = document.getElementById('file-progress');
+
+            if (e.lengthComputable) {
+                pBar.max = e.total;
+            }
+        }
+
+        function updateFileProgress(e) {
+            var pBar = document.getElementById('file-progress');
+
+            if (e.lengthComputable) {
+                pBar.value = e.loaded;
+            }
+        }
+
+        function uploadFile(file) {
+
+            var xhr = new XMLHttpRequest(),
+                fileInput = document.getElementById('class-roster-file'),
+                pBar = document.getElementById('file-progress'),
+                fileSizeLimit = 1024; // In MB
+            if (xhr.upload) {
+                // Check if file is less than x MB
+                if (file.size <= fileSizeLimit * 1024 * 1024) {
+                    // Progress bar
+                    pBar.style.display = 'inline';
+                    xhr.upload.addEventListener('loadstart', setProgressMaxValue, false);
+                    xhr.upload.addEventListener('progress', updateFileProgress, false);
+
+                    // File received / failed
+                    xhr.onreadystatechange = function(e) {
+                        if (xhr.readyState == 4) {
+                            // Everything is good!
+
+                            // progress.className = (xhr.status == 200 ? "success" : "failure");
+                            // document.location.reload(true);
+                        }
+                    };
+
+                    // Start upload
+                    xhr.open('POST', document.getElementById('file-upload-form').action, true);
+                    xhr.setRequestHeader('X-File-Name', file.name);
+                    xhr.setRequestHeader('X-File-Size', file.size);
+                    xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+                    xhr.send(file);
+                } else {
+                    output('Please upload a smaller file (< ' + fileSizeLimit + ' MB).');
+                }
+            }
+        }
+
+        // Check for the various File API support.
+        if (window.File && window.FileList && window.FileReader) {
+            Init();
+        } else {
+            document.getElementById('file-drag').style.display = 'none';
+        }
+    }
+    ekUpload();
+</script>
